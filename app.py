3c0ef86bddcee43c6ffa7b92fc5472a728dc44ec
Mike Firer <firerml@gmail.com>
2017-02-18 15:20:05 -0500

    init

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+
+def _shapes():
+    return {
+        'box': (lambda emoji: ((emoji*3 + '\n')*3)[:-1])
+    }
+
+
+def make_shape(shape_name, emoji):
+    func = _shapes().get(shape_name)
+    if func:
+        return func(emoji)
+    return ''
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello, world."
+
+
+@app.route('/get_shape', methods=['POST'])
+def get_shape():
+    command_words = request.form['command'].split(' ')
+    shape = command_words[1]
+    result = make_shape(shape, command_words[2])
+    if not result:
+        return 'BAD'
+    return Response(result, mimetype='text/plain')
+
+if __name__ == "__main__":
+    app.run(debug=True)
