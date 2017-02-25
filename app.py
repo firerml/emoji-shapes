f19b07cac5d9f7fcfaba02f801f1c0cefe32cbdb
Mike Firer <firerml@gmail.com>
2017-02-25 01:48:51 -0500

    Database with encrypted user tokens

--- app.py
+++ app.py
@@ -3,51 +3,56 @@ import os
 from flask import Flask, Response, request, jsonify
 import requests
 
-import db
+from db import DB
 from shape_maker import get_list, make_shape, ShapeException
 
 app = Flask(__name__)
 
-CLIENT_ID = '49173439911.142714893344'
 RESPONSE_TYPE_IN_CHANNEL = "in_channel"
 RESPONSE_TYPE_EPHEMERAL = "ephemeral"
 
-# AUTH_STATE = os.environ.get('ARTMOJI_OAUTH_STATE', '')
-AUTH_STATE = 'MAJKmjG8V5HeavKZ'
+AUTH_STATE = os.environ.get('ARTMOJI_OAUTH_STATE', '')
+CLIENT_ID = os.environ.get('ARTMOJI_CLIENT_ID', '')
+CLIENT_SECRET = os.environ.get('ARTMOJI_CLIENT_SECRET', '')
+
+ERROR_MESSAGE = 'Oops! There was an error. You can email me at firerml (at) gmail (dot) com to report this bug.'
 
 
 @app.route('/oauth', methods=['GET'])
 def oauth():
+    # Request and save user's auth token, which will be used to post on the user's behalf.
     if not request.args.get('state') == AUTH_STATE:
-        return jsonify({'ok': False, 'error': 'OAuth state does not match'})
-    return jsonify({'ok': True, 'code': request.args['code']})
+        return Response(ERROR_MESSAGE)
+    request.args.get('code')
+    res_data = requests.get('https://slack.com/api/oauth.access', params={
+        'client_id': '49173439911.142714893344',
+        'client_secret': '4a8b3da1505c3d80c6320c675665a6e9',
+        'code': request.args.get('code', '')
+    }).json()
+    success = False
+    if res_data['ok']:
+        success = DB.add_token(res_data['user_id'], res_data['access_token'])
+
+    if not success:
+        return Response(ERROR_MESSAGE)
+    return 'Great! You\'ve authorized Artmoji to post on your behalf in {}.\n'.format(res_data['team_name']) +\
+           'You can close this page and go back to Slack to post beautiful Artmojis. :)'
 
 
 @app.route('/get_shape', methods=['POST'])
 def get_shape():
 
-    print(request.form)
-
-    user_auth_token = db.get_token_str(request.form['user_id'])
+    user_auth_token = DB.get_token_str(request.form['user_id'])
 
     if not user_auth_token:
         oauth_url = 'https://slack.com/oauth/authorize?client_id={}&scope=chat:write:user&team={}&state={}'
         oauth_url = oauth_url.format(CLIENT_ID, request.form['team_id'], AUTH_STATE)
-        print(oauth_url)
-
-        token_code = ''
         return jsonify({
-            'text': 'Authorize Artmoji to post on your behalf:\n' + oauth_url,
+            'text': 'Authorize Artmoji to post on your behalf:\n' +
+                    '(This will never be used for anything but posting artmojis at your request!)\n' +
+                    oauth_url,
             'response_type': RESPONSE_TYPE_EPHEMERAL
         })
-        # res = requests.get(oauth_url)
-        #
-        # if res.ok:
-        #     res_data = res.json()
-        #     if res_data['ok']:
-        #         token_code = res_data.get('code')
-        # if not token_code:
-        #     return Response(status=400)
 
     text_words = request.form['text'].split(' ')
     shape = text_words[0]
@@ -68,12 +73,14 @@ def get_shape():
     if request.args.get('format') == 'text':
         return result
 
+    if response_type == RESPONSE_TYPE_EPHEMERAL:
+        return jsonify({'text': result, 'response_type': response_type})
+
+    # Post on the user's behalf.
     requests.post('https://slack.com/api/chat.postMessage', json={
-        'token': token_code, 'channel': request.form['channel_id'], 'text': result
+        'token': user_auth_token, 'channel': request.form['channel_id'], 'text': result
     })
-    # Keeps the user's slash command from showing up.
-    # requests.post(request.form['response_url'], json={'text': result, 'response_type': response_type})
-    # return Response(status=200)
+    return Response(status=200)
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,44 +1,85 @@
+import base64
+import logging
+import os
+
+from Crypto.Cipher import AES
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+ENCRYPTION_KEY = os.environ.get('ARTMOJI_ENCRYPTION_KEY', '1234567890123456')
+BLOCK_SIZE = 16
+PADDING = '{'
+
+DB_URL = os.environ.get('DATABASE_URL', 'sqlite:///artmoji.db')
 Base = declarative_base()
 
 
 class Token(Base):
-    __tablename__ = 'token'
+    __tablename__ = 'tokens'
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(30), nullable=False, index=True, unique=True)
-    token = Column(String(30), nullable=False)
+    user_id = Column(String(40), nullable=False, index=True, unique=True)
+    token = Column(String(250), nullable=False)
 
 
-engine = create_engine('sqlite:///artmoji.db')
-DB = sessionmaker(bind=engine)()
-if not engine.dialect.has_table(engine, Token.__tablename__):
-    Base.metadata.create_all(engine)
+class Database:
+    def __init__(self):
+        engine = create_engine(DB_URL)
+        self.sessionmaker = sessionmaker(bind=engine)
+        self._session = self.sessionmaker()
+
+        # Create Token table if it does not exist.
+        if not engine.dialect.has_table(engine, Token.__tablename__):
+            Base.metadata.create_all(engine)
+
+    @property
+    def session(self):
+        if not self._session.is_active:
+            self._session = self.sessionmaker()
+        return self._session
+
+    def commit(self):
+        session = self.session
+        try:
+            session.commit()
+            return True
+        except Exception as e:
+            logging.error(e)
+            session.rollback()
+            return False
+
+    def get_token_obj(self, user_id):
+        token_rows = self.session.query(Token).filter(Token.user_id == user_id).all()
+        if token_rows:
+            return token_rows[0]
+        return None
+
+    def get_token_str(self, user_id):
+        token_obj = self.get_token_obj(user_id)
+        if token_obj:
+            return decrypt_token(token_obj.token)
+        return ''
+
+    def add_token(self, user_id, token):
+        # Returns success boolean
+        self.session.add(Token(user_id=user_id, token=encrypt_token(token)))
+        return self.commit()
+
+    def delete_token(self, user_id):
+        # Returns success boolean
+        token_obj = self.get_token_obj(user_id)
+        if token_obj:
+            self.session.delete(token_obj)
+            return self.commit()
+
+DB = Database()
 
 
-def get_token_obj(user_id):
-    token_rows = DB.query(Token).filter(Token.user_id == user_id).all()
-    if token_rows:
-        return token_rows[0]
-    return None
+def encrypt_token(token):
+    padded_token = token + (BLOCK_SIZE - len(token) % BLOCK_SIZE) * PADDING
+    return base64.b64encode(AES.new(ENCRYPTION_KEY).encrypt(padded_token)).decode()
 
 
-def get_token_str(user_id):
-    token_obj = get_token_obj(user_id)
-    if token_obj:
-        return token_obj.token
-    return ''
-
-
-def add_token(user_id, token):
-    DB.add(Token(user_id=user_id, token=token))
-    DB.commit()
-
-
-def delete_token(user_id):
-    token_obj = get_token_obj(user_id)
-    if token_obj:
-        DB.delete(token_obj)
+def decrypt_token(encrypted_token):
+    return AES.new(ENCRYPTION_KEY).decrypt(base64.b64decode(encrypted_token.encode())).decode().rstrip(PADDING)
